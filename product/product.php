349a0eeb9794ff6457ae8c2a0a2a83df4922bd29
howardlai1234 <48821491+howardlai1234@users.noreply.github.com>
2021-06-18 00:22:00 +0800

    Add files via upload
    uploaded files on directory css/, fonts/, images/ and js/

---
+++ product/product.php
@@ -0,0 +1,244 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en"><!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+    <title>Foods-fushiony</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">    
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="../css/style.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="../css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	<!-- Start header -->
+	<header class="top-navbar">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="../menu.php">
+					<img src="../images/logo.png" alt="" width="100" height="100"/>
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbars-rs-food">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item"><a class="nav-link" href="../menu.php">Menu</a></li>
+						<li class="nav-item"><a class="nav-link" href="../shopping-cart.php">Shopping Cart</a></li>
+						<li class="nav-item"><a class="nav-link" href="../account.php">Account</a></li>
+						<li class="nav-item"><a class="nav-link" href="../logout.php">Logout</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+	<?php
+		include("../product-config.php");
+		echo $_GET['productname'];
+		if (!empty($_GET['productname'])) {
+			$productname= $_GET['productname'];
+		}
+		else{
+			$productname = "Barbecued Pork Rice";
+		}
+		$sql = "SELECT Product.productID, Product.productName, ProductType.typeName, ProductOrigin.locationName, Product.description, Product.price, Product.ingredients, Product.imageURL, Product.stock FROM Product, ProductOrigin, ProductType WHERE Product.typeID=ProductType.typeID AND Product.originID=ProductOrigin.locationID AND Product.productName='$productname' ;";
+		$result = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_array($result);
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$_SESSION['qty'] = $_POST['qty'];
+			$_SESSION['productID'] = $row['productID'];
+			header("Location: ../shopping-cart.php");
+		}
+	?>
+
+	<!-- Start All Pages -->
+	<div class="all-page-title page-breadcrumb">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1><?php echo $row['typeName']; ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End All Pages -->
+		
+	<!-- Start product -->
+	<div class="product-box">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="heading-title text-center">
+					<h2><br><?php echo $row['productName']; ?><br></h2>
+					<img src="..<?php echo $row['imageURL']; ?>" class="img-fluid" alt="Image" width="320" height="240">
+					<h3><?php echo $row['description']; ?></h3>
+					<h2 style='color:orangered;'>$<?php echo $row['price']?></h2>
+					<h3> <br>Country of Origin:<br></h3>
+					<h4><?php echo $row['locationName']?></h4>
+					<h3> <br>Ingredients:<br></h3>
+					<h4><?php echo $row['ingredients']?></h4>
+					<?php if ($row['stock'] >= 10) { ?>
+					<form id="orderForm" action="" method = "post">
+						<label for="qty">Quantity:</label>
+						<select name="qty" id="qty">
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+						</select>
+						<button class="btn btn-common" id="submit" type="submit">Order</button>
+					</form>
+					<?php }else{ ?>
+						<h2 style='color:orangered;'>Sorry, this porduct is currently out of stock,<br>We apologise for any inconvenience</h2>
+					<?php } ?>
+					<button class="btn btn-common" onclick="document.location='../menu.php'">Return to Menu</button>
+				</div>
+			</div>
+		</div>		
+	</div>
+	<!-- End product -->
+	
+	<!-- Start Contact info -->
+	<div class="contact-imfo-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-volume-control-phone"></i>
+					<div class="overflow-hidden">
+						<h4>Phone</h4>
+						<p class="lead">
+							3456 7890
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-envelope"></i>
+					<div class="overflow-hidden">
+						<h4>Email</h4>
+						<p class="lead">
+							fushiony@gmail.com
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-map-marker"></i>
+					<div class="overflow-hidden">
+						<h4>Location</h4>
+						<p class="lead">
+							11 Yuk Choi Rd, Hung Hom, Hong Kong
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact info -->
+	
+	<!-- Start Footer -->
+		<footer class="footer-area bg-f">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<h3>About Us</h3>
+					<p>
+					Every meal is fully pack with nutrients designed by our passionate professional chefs and conveniently delivered to you.
+					We want to help you feel your best during your busy work schedule by serving you with
+					all natural and delicious meals free from any preservatives and MSG.
+					We pay full attention in every ingredients that we select for each meal
+					and we don't add any more or any less than what is is necessary for the meal.</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Subscribe</h3>
+					<div class="subscribe_form">
+						<form class="subscribe_form">
+							<input name="EMAIL" id="subs-email" class="form_input" placeholder="Email Address..." type="email">
+							<button type="submit" class="submit">SUBSCRIBE</button>
+							<div class="clearfix"></div>
+						</form>
+					</div>
+					<ul class="list-inline f-social">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Contact information</h3>
+					<p class="lead">11 Yuk Choi Rd, Hung Hom, Hong Kong</p>
+					<p class="lead"><a href="#">3456 7890 </a></p>
+					<p><a href="#"> info@admin.com</a></p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Opening hours</h3>
+					<p><span class="text-color">Monday: </span> 9:30 A.M. - 8 P.M.</p>
+					<p><span class="text-color">Tue-Wed :</span> 9:30 A.M. - 8 P.M.</p>
+					<p><span class="text-color">Thu-Fri :</span> 9:30 A.M. - 8 P.M.</p>
+					<p><span class="text-color">Sat-Sun :</span> 11:30 A.M. - 6 P.M.</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<p class="company-name">All Rights Reserved. &copy; 2020 <a href="#">Fushiony</a>
+					</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
+
+	<!-- ALL JS FILES -->
+	<script src="../js/jquery-3.2.1.min.js"></script>
+	<script src="../js/popper.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	
+	<script src="../js/jquery.superslides.min.js"></script>
+	<script src="../js/images-loded.min.js"></script>
+	<script src="../js/isotope.min.js"></script>
+	<script src="../js/baguetteBox.min.js"></script>
+	<script src="../js/form-validator.min.js"></script>
+    <script src="../js/contact-form-script.js"></script>
+    <script src="../js/custom.js"></script>
+</body>
+</html>
